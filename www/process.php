db0843172a94c1800f754f81efd5050bbc3d1386
limongus <145297656+limongus@users.noreply.github.com>
2025-10-10 15:22:44 +0200

    Add: saving form entries to data.txt

--- www/process.php
+++ www/process.php
@@ -13,6 +13,12 @@ $_SESSION['form_data'] = [
     'email' => $email
 ];
 
+// Собираем все данные формы в одну строку, разделяя их точкой с запятой
+$line = date("Y-m-d H:i:s") . ";" . $fullName . ";" . $email . "\n";
+
+// Дописываем строку в конец файла data.txt
+file_put_contents("data.txt", $line, FILE_APPEND);
+
 // 4. Перенаправляем пользователя на главную страницу
 header("Location: index.php");
 exit(); // Завершаем выполнение скрипта после перенаправления
